ea17f170b7fd4727e62bb7eef9c194097a757661
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-19 13:16:26 -0700

    build

--- server.js
+++ server.js
@@ -33,14 +33,14 @@ db().then((connection) => {
 });
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client"));
+  app.use(express.static("client/build"));
 }
 
 app.use("/api/inquire", require("./routes/inquire"));
 app.use("/api/sendEmail", require("./routes/email"));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client"));
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => {
