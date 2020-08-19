55409c48a74af841e33d34e9aa3c8ae4011f8d7c
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-08-19 15:55:34 -0700

    photo update

--- client/src/components/AboutStaffCard/index.js
+++ client/src/components/AboutStaffCard/index.js
@@ -3,16 +3,20 @@ import "./styles.css";
 
 function AboutStaffCard(props) {
   return (
-    <div className="col-sm-12">
-      <li>
-        <div class="card" style="width: 18rem;">
-          <img class="card-img-top" src={props.image} alt="Card image cap" />
+    <div className="col-sm-8 mb-5  ">
+      <li className="align-items-center">
+        <div className="card" styles="width: 18rem;">
+          <img
+            className="card-img-top about-picture"
+            src={props.image}
+            alt="Card image cap"
+          />
           <div className="card-header">
             {props.name},{"  "}
             {props.title}
           </div>
-          <div class="card-body">
-            <p class="card-text">{props.about}</p>
+          <div className="card-body">
+            <p className="card-text">{props.about}</p>
           </div>
         </div>
       </li>
